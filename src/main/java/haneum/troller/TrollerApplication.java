6ce17b1a070edc6ef7b874f7d13b2ff896143e52
gyuturn <rbtjrrbals@gmail.com>
2022-06-12 15:13:50 +0900

    setting

---
+++ src/main/java/haneum/troller/TrollerApplication.java
@@ -0,0 +1,13 @@
+package haneum.troller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrollerApplication.class, args);
+	}
+
+}
