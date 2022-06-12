e28713b6289f52935547c024ba2434976a28324f
turn <kksy2423@naver.com>
2022-06-13 01:25:17 +0900

    feat: 회원가입 model

---
+++ src/main/java/haneum/troller/form/SignUpForm.java
@@ -0,0 +1,15 @@
+package haneum.troller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignUpForm {
+
+    private String eMail;
+
+    private String password;
+
+    private String lolName;
+}
