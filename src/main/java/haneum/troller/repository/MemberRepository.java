4afde725c80d51384a1d96f7ad6ac60b7c172afe
turn <kksy2423@naver.com>
2022-06-14 03:11:59 +0900

    feat: 아이디를 통해 비밀번호 일치여부 확인

--- src/main/java/haneum/troller/repository/MemberRepository.java
+++ src/main/java/haneum/troller/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,7 +20,15 @@ public class MemberRepository {
         return em.find(Member.class, memberId);
     }
 
-
+    public Member findByEmail(String email){
+        List<Member> memberList = em.createQuery("select m from Member m where m.eMail=:eMail", Member.class)
+                .setParameter("eMail", email)
+                .getResultList();
+        if(memberList.size()!=1){
+            throw new IllegalStateException("email과 일치하는 member가 없습니다.");
+        }
+        return memberList.get(0);
+    }
 
 
 }
