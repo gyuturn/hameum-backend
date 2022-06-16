aad99e3aecf8b67631875152526619072c7f8646
turn <kksy2423@naver.com>
2022-06-17 03:41:56 +0900

    refactor: LoginForm 생성자 생성

--- src/main/java/haneum/troller/dto/LoginForm.java
+++ src/main/java/haneum/troller/dto/LoginForm.java
@@ -9,4 +9,9 @@ public class LoginForm {
     private String eMail;
 
     private String password;
+
+    public LoginForm(String eMail, String password) {
+        this.eMail = eMail;
+        this.password = password;
+    }
 }
