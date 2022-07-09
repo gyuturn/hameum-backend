24d784e038214397161cf739b12e813fe754f5cc
gyuturn <rbtjrrbals@gmail.com>
2022-07-09 13:56:19 +0900

    refactor: @SpringBootApplication 수정

--- src/main/java/haneum/troller/TrollerApplication.java
+++ src/main/java/haneum/troller/TrollerApplication.java
@@ -3,10 +3,17 @@ package haneum.troller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TrollerApplication {
+public class TrollerApplication extends SpringBootServletInitializer
+{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TrollerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrollerApplication.class, args);
