428b93142ebef9bee6ad7f148a74793f5b18f3fd
gyuturn <rbtjrrbals@gmail.com>
2022-07-16 18:04:59 +0900

    refactor: config 파일 경로 수정

--- src/main/java/haneum/troller/common/config/WebSecurityConfig.java
+++ src/main/java/haneum/troller/common/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package haneum.troller.config;
+package haneum.troller.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
