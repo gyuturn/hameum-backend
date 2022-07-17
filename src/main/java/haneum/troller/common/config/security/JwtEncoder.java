35e72ea99a7912c47a2c984ce3080ba82e9d73d8
gyuturn <rbtjrrbals@gmail.com>
2022-07-18 00:14:42 +0900

    feat: refresh token api

--- src/main/java/haneum/troller/common/config/security/JwtEncoder.java
+++ src/main/java/haneum/troller/common/config/security/JwtEncoder.java
@@ -1,8 +1,7 @@
-package haneum.troller.security;
+package haneum.troller.common.config.security;
 
 import haneum.troller.domain.Member;
 import haneum.troller.repository.MemberRepository;
-import haneum.troller.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -15,20 +14,27 @@ import java.security.Key;
 import java.util.Date;
 
 @Service
-public class SecurityService {
-    private  String secretKey;
+public class JwtEncoder {
+    private static final String secretKey="GGeokDrupakdlsdkqwdkdfdaddjflkdwodfdasdasdafsdfeflwfqvfdmfdsfdkjaslfjisdfjosidf";
     @Autowired
     MemberRepository memberRepository;
 
-    //로그인 서비스 던질때 같이
-    public String createToken(String eMail,long expTime) {
+
+    public String createAccessToken(Long id,long expTime){
         if (expTime <= 0) {
             throw new RuntimeException("만료시간이 0보다 커야함");
         }
 
-        Member member = memberRepository.findByEmail(eMail);
-        secretKey = member.getPassword();
+        return getJwtToken(String.valueOf(id), expTime);
+    }
+    public String createRefreshToken(String eMail, long expTime) {
+        if (expTime <= 0) {
+            throw new RuntimeException("만료시간이 0보다 커야함");
+        }
+        return getJwtToken(eMail, expTime);
+    }
 
+    private String getJwtToken(String subject, long expTime) {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
         byte[] secretKeyByte = DatatypeConverter.parseBase64Binary(secretKey);
 
@@ -36,35 +42,35 @@ public class SecurityService {
         Key singingKey = new SecretKeySpec(secretKeyByte, signatureAlgorithm.getJcaName());
 
         return Jwts.builder()
-                .setSubject(eMail)
+                .setSubject(subject)
                 .signWith(singingKey, signatureAlgorithm)
                 .setExpiration(new Date(System.currentTimeMillis() + expTime))
                 .compact();
     }
 
 
-    public String getEmailByToken(String token) {
-        String eMail = null;
+    public String getSubjectByToken(String token) {
+        String subject = null;
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(DatatypeConverter.parseBase64Binary(secretKey))
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-        eMail = claims.getSubject();
+        subject = claims.getSubject();
 
-        return eMail;
+        return subject;
 
     }
 
     //토큰 검증 메서드를 boolean
     public boolean validToken(String token) {
-        String email = getEmailByToken(token);
+        String email = getSubjectByToken(token);
         if (email!=null) return true;
         else return false;
     }
 
     public String findLolNameByToken(String token) throws IllegalAccessException {
-        String email = getEmailByToken(token);
+        String email = getSubjectByToken(token);
         Member member = memberRepository.findByEmail(email);
         return member.getLolName();
     }
