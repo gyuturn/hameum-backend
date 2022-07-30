6260e6a475ce154c44c28ed02a367772aefbec44
gyuturn <rbtjrrbals@gmail.com>
2022-07-30 16:51:24 +0900

    refactor: 필요없는 import 수정

--- src/main/java/haneum/troller/controller/jwtToken/AccessTokenResController.java
+++ src/main/java/haneum/troller/controller/jwtToken/AccessTokenResController.java
@@ -1,24 +1,13 @@
 package haneum.troller.controller.jwtToken;
 
-import haneum.troller.common.config.security.JwtEncoder;
-import haneum.troller.domain.Member;
-import haneum.troller.dto.jwtDto.JwtDto;
+import haneum.troller.common.security.JwtEncoder;
 import haneum.troller.repository.MemberRepository;
 import io.jsonwebtoken.ExpiredJwtException;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
