063c9d39a74a90ebd63439626a9d3ac62e5b0e62
turn <kksy2423@naver.com>
2022-08-07 01:45:06 +0900

    refactor: dir 이름 변경

--- src/main/java/haneum/troller/controller/jwtToken/AccessTokenResController.java
+++ src/main/java/haneum/troller/controller/jwtToken/AccessTokenResController.java
@@ -1,6 +1,6 @@
 package haneum.troller.controller.jwtToken;
 
-import haneum.troller.common.security.JwtEncoder;
+import haneum.troller.service.security.JwtService;
 import haneum.troller.repository.MemberRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 public class AccessTokenResController {
-    private final JwtEncoder jwtEncoder;
+    private final JwtService jwtService;
     private final MemberRepository memberRepository;
 
     public ResponseEntity getSubject(@RequestHeader(value = "accessToken") String token) {
         log.info("access-token 인증");
         try {
-            jwtEncoder.getSubjectByToken(token);
+            jwtService.getSubjectByToken(token);
         } catch (ExpiredJwtException e) {
             log.debug("access-token 만료");
             return new ResponseEntity(HttpStatus.FORBIDDEN);
