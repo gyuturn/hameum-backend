212a4e014da1f95eddb51442cf774d4895a515ea
turn <kksy2423@naver.com>
2022-08-06 23:17:46 +0900

    feat: 카카로 로그인 로직수정

--- src/main/java/haneum/troller/service/login/KaKaoLoginService.java
+++ src/main/java/haneum/troller/service/login/KaKaoLoginService.java
@@ -18,59 +18,60 @@ public class KaKaoLoginService {
         String refresh_Token = "";
         String reqURL = "https://kauth.kakao.com/oauth/token";
 
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        URL url = new URL(reqURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            //    POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
 
-        //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
+            //    POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=5d497d3a5b84df801c7913fd22e153b9");
+            sb.append("&redirect_uri=http://3.37.22.89:3000/auth/kakao");
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
 
-        //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-        StringBuilder sb = new StringBuilder();
-        sb.append("grant_type=authorization_code");
-        sb.append("&client_id= 5d497d3a5b84df801c7913fd22e153b9"); // TODO REST_API_KEY 입력
-        sb.append("&redirect_uri=http://3.37.22.89:3000/auth/kakao"); // TODO 인가코드 받은 redirect_uri 입력
-        sb.append("&code=" + code);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+            //    결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
 
-        //결과 코드가 200이라면 성공
-        int responseCode = conn.getResponseCode();
-//        if (!(responseCode == 200)) {
-//            throw new KakaoLoginException("카카오 인증서버와의 로직에서 에러");
-//        }
-        //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = "";
-        String result = "";
+            //    요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
 
-        while ((line = br.readLine()) != null) {
-            result += line;
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //    Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+            System.out.println("access_token : " + access_Token);
+            System.out.println("refresh_token : " + refresh_Token);
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        System.out.println("response body : " + result);
-
-        //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(result);
-
-        access_Token = element.getAsJsonObject().get("access_token").getAsString();
-        refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
-
-        System.out.println("access_token -kakao: " + access_Token);
-        System.out.println("refresh_token -kakao : " + refresh_Token);
-
-        br.close();
 
 
-        JwtDto jwtDto = JwtDto.builder()
+        return JwtDto.builder()
                 .accessToken(access_Token)
                 .refreshToken(refresh_Token)
                 .build();
-
-        return jwtDto;
     }
 
 
