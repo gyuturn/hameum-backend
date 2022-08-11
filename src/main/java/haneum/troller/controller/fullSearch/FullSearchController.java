91cd71f663ab524c4b8b4c80b0e4ab48294c4f42
turn <kksy2423@naver.com>
2022-08-11 23:57:13 +0900

    feat: swagger 명세 구성

--- src/main/java/haneum/troller/controller/fullSearch/FullSearchController.java
+++ src/main/java/haneum/troller/controller/fullSearch/FullSearchController.java
@@ -1,11 +1,17 @@
 package haneum.troller.controller.fullSearch;
 
+import haneum.troller.common.exception.exceptions.LolApiToJsonException;
 import haneum.troller.dto.gameRecord.GameRecordDto;
 import haneum.troller.dto.linePrefer.LinePreferenceDto;
 import haneum.troller.dto.mostChampion.MostThreeChampionDto;
 import haneum.troller.dto.myPage.MyPageDto;
+import haneum.troller.service.GameRecordService;
+import haneum.troller.service.LinePreferenceService;
+import haneum.troller.service.MostThreeChampionService;
 import haneum.troller.service.MyPageService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,6 +20,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.text.ParseException;
 
 @Tag(name="userGameRecord",description = "유저의 게임전적 조회시 사옹되는 API")
 @RestController
@@ -47,44 +56,66 @@ public class FullSearchController {
         }
         return new ResponseEntity(myPageDtoFinal,HttpStatus.OK);
     }
-    @GetMapping("line")
-    public ResponseEntity getTokenForLine(@RequestParam(value = "lolName") String lolName){
-        LinePreferenceDto linePreferenceDto= null;
-        try{
+
+    @Operation(summary = "유저의 라인정보(포지션) api", description = "유저의 line정보와 해당 라인에 대한 판수를 json return")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200",description = "정상적 조회"),
+                    @ApiResponse(responseCode = "404",description = "해당 유저가 없음"),
+                    @ApiResponse(responseCode = "503",description = "롤 api 호출시 로직에러")
+            }
+    )
+    @Parameter(name="lolName",description = "롤네임")
+    @GetMapping("user/line")
+    public ResponseEntity getTokenForLine(@RequestParam(value = "lolName") String lolName) throws LolApiToJsonException {
+        LinePreferenceDto linePreferenceDto = null;
+        try {
             linePreferenceDto = linePreferenceService.getLinePreferenceDto(lolName);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new LolApiToJsonException("롤 api에 호출시 에러");
         }
         return new ResponseEntity(linePreferenceDto, HttpStatus.OK);
     }
-    @GetMapping("most")
-    public ResponseEntity getTokenForMost(@RequestParam(value = "lolName") String lolName){
+
+
+
+    @Operation(summary = "유저의 모스트 챔피언 api", description = "유저의 most챔피언과 그에 해당하는 전적정보를 알려줌")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200",description = "정상적 조회"),
+                    @ApiResponse(responseCode = "404",description = "해당 유저가 없음"),
+                    @ApiResponse(responseCode = "503",description = "롤 api 호출시 로직에러")
+            }
+    )
+    @Parameter(name="lolName",description = "롤네임")
+    @GetMapping("usre/most")
+    public ResponseEntity getTokenForMost(@RequestParam(value = "lolName") String lolName) throws LolApiToJsonException {
         MostThreeChampionDto mostThreeChampionDto = null;
         try{
             mostThreeChampionDto = mostThreeChampionService.getMostThreeChampionDto(lolName);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new LolApiToJsonException("롤 api에 호출시 에러");
         }
         return new ResponseEntity(mostThreeChampionDto, HttpStatus.OK);
     }
-    @GetMapping("gameRecord")
-    public ResponseEntity getTokenForGameRecord(@RequestParam(value = "lolName") String lolName){
+
+
+    @Operation(summary = "유저의 20게임 전적정보 api", description = "유저의 20게임에 대한 전적정보를 알려줌")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200",description = "정상적 조회"),
+                    @ApiResponse(responseCode = "404",description = "해당 유저가 없음"),
+                    @ApiResponse(responseCode = "503",description = "롤 api 호출시 로직에러")
+            }
+    )
+    @Parameter(name="lolName",description = "롤네임")
+    @GetMapping("user/gameRecord")
+    public ResponseEntity getTokenForGameRecord(@RequestParam(value = "lolName") String lolName) throws LolApiToJsonException {
         GameRecordDto gameRecordDto = null;
-        try{
+        try {
             gameRecordDto = gameRecordService.getGameRecord(lolName);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        } catch (java.text.ParseException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new LolApiToJsonException("롤 api에 호출시 에러");
         }
         return new ResponseEntity(gameRecordDto, HttpStatus.OK);
     }
