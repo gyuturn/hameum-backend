1891bba8718d61c0e6d08ec1eccc2745ff335050
jeongmino <jeongmino1207@gmail.com>
2022-09-03 00:54:22 +0900

    fix : csPerMinutes 철자, item, itemImg 변수 이름 수정 fix : ChampionImgService url 오타 수정 fix : itemImg url 최신화

--- src/main/java/haneum/troller/service/dataDragon/ChampionImgService.java
+++ src/main/java/haneum/troller/service/dataDragon/ChampionImgService.java
@@ -7,7 +7,7 @@ public class ChampionImgService {
 
     public String getChampionImg(String championName)  {
         String url = "https://ddragon.leagueoflegends.com/cdn/"
-                + "10.6.1"
+                + "12.15.1"
                 + "/img/champion/"
                 + championName
                 + ".png";
