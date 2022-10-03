b89fa5ad9a65e35c4fb1c49a697bf2e5e6a13df3
gyuturn <rbtjrrbals@gmail.com>
2022-10-03 18:31:17 +0900

    feat: 전적검색 update rlsmdcnrk

---
+++ src/main/java/haneum/troller/dto/dataflow/LolNameDto.java
@@ -0,0 +1,15 @@
+package haneum.troller.dto.dataflow;
+
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Builder
+public class LolNameDto {
+    private String lolName;
+}
